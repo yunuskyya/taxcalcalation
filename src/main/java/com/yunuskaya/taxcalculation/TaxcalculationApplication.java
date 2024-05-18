e2277edcf56459517efda68e3607e1150277d0b7
yunuskyya <0yunuskaya0@gmail.com>
2024-05-18 17:48:34 +0300

    maven file installed.

---
+++ src/main/java/com/yunuskaya/taxcalculation/TaxcalculationApplication.java
@@ -0,0 +1,13 @@
+package com.yunuskaya.taxcalculation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxcalculationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxcalculationApplication.class, args);
+	}
+
+}
